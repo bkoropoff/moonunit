26b8746059b591bcf847b9da5f681db6f2f4d748
Brian Koropoff <bkoropoff@gmail.com>
2008-02-20 03:32:19 +0000

    Add usage information to moonunit-strip
    git-svn-id: https://svn.code.sf.net/p/moonunit/svn/trunk/moonunit@61 7cfde7b9-103a-0410-917d-a4b0b378e324

--- src/moonunit/moonunit-strip.sh
+++ src/moonunit/moonunit-strip.sh
@@ -1,11 +1,49 @@
 #!/bin/bash
 
-for file in "$@"
+arg="$1"
+
+usage()
+{
+    local name=$(basename $1)
+    cat << __EOF__
+$name -- remove MoonUnit tests from a library or object file
+
+  This script strips a library or object file of data and code
+  sections containing MoonUnit tests, as well as all strippable
+  related symbols.
+
+Usage: $name [options] <file1>...
+    -?, -h, --help        Display this usage information
+__EOF__
+}
+
+while [ -n "$arg" ]
 do
-	objcopy -w \
-		-R ".moonunit_text" -R ".moonunit_data" \
-		-N '__mu_t_*'       -N '__mu_f_*' \
-		-N '__mu_fs_*'      -N '__mu_ft_*' \
-		-N '__mu_ls'        -N '__mu_lt' \
-		$file
+    shift
+    case "$arg" in
+        -?|-h|--help)
+            usage "$0"
+            exit 0
+            ;;
+        *)
+            files=("${files[@]}" "$arg")
+            ;;
+    esac
+    arg="$1"
 done
+
+if [ -n "$files" ]
+then
+    for file in "${files[@]}"
+    do
+        objcopy -w \
+            -R ".moonunit_text" -R ".moonunit_data" \
+            -N '__mu_t_*'       -N '__mu_f_*' \
+            -N '__mu_fs_*'      -N '__mu_ft_*' \
+            -N '__mu_ls'        -N '__mu_lt' \
+            $file
+    done
+else
+    usage "$0"
+    exit 1
+fi
